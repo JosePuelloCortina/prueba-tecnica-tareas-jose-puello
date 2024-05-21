0ce4ae2ec3cf0d8c2e3af4d9d5e4fa866d7014b2
Jose Puello <puelloj66@gmail.com>
2024-05-21 13:31:09 -0500

    feat: agregar funcionalidad de eliminar tareas

--- server/src/controllers/taskController/deleteTask.js
+++ server/src/controllers/taskController/deleteTask.js
@@ -6,13 +6,13 @@ const deleteTask = async (req, res) => {
         const task = await Task.findByPk(id);
 
         if (!task) {
-            return res.status(404).json({ message: "Task not found" });
+            return res.status(404).json({ message: "Tarea no encontrada" });
         }
         await task.destroy();
-        return res.status(200).json({ message: "Task deleted successfully" });
+        return res.status(200).json({ message: "Tarea eliminada exitosamente" });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Error deleting task" });
+        res.status(500).json({ error: "Error delete Tarea" });
     }
 };
 
--- server/src/routes/taskRoutes/task.routes.js
+++ server/src/routes/taskRoutes/task.routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const getAllTasks = require("../../controllers/taskController/getAllTasks");
 const createTask = require("../../controllers/taskController/createTask");
 const updateTask = require("../../controllers/taskController/updateTask");
+const deleteTask = require('../../controllers/taskController/deleteTask');
 
 
 const router = Router(); 
@@ -9,6 +10,7 @@ const router = Router();
 router.get('/gettasks', getAllTasks);
 router.post('/createtask/user/:id', createTask); 
 router.put('/updatetask/task/:id', updateTask);
+router.delete('/deletetask/task/:id', deleteTask)
 
 
 module.exports = router; 
