947aed7fee81bd3fe25c92ce55edd3cd123163fb
Jose Puello <puelloj66@gmail.com>
2024-05-21 13:40:03 -0500

    feat: permisos de autorizacion a las rutas de tareas

--- server/src/routes/taskRoutes/task.routes.js
+++ server/src/routes/taskRoutes/task.routes.js
@@ -3,14 +3,15 @@ const getAllTasks = require("../../controllers/taskController/getAllTasks");
 const createTask = require("../../controllers/taskController/createTask");
 const updateTask = require("../../controllers/taskController/updateTask");
 const deleteTask = require('../../controllers/taskController/deleteTask');
+const requireAuth = require('../../middlewares/requireAuth');
 
 
 const router = Router(); 
 
-router.get('/gettasks', getAllTasks);
-router.post('/createtask/user/:id', createTask); 
-router.put('/updatetask/task/:id', updateTask);
-router.delete('/deletetask/task/:id', deleteTask)
+router.get('/gettasks', requireAuth, getAllTasks);
+router.post('/createtask/user/:id', requireAuth, createTask); 
+router.put('/updatetask/task/:id', requireAuth, updateTask);
+router.delete('/deletetask/task/:id', requireAuth, deleteTask)
 
 
 module.exports = router; 
