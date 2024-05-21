fe0e047db6358c60d881ad7fd19457ecf7fc8227
Jose Puello <puelloj66@gmail.com>
2024-05-21 08:58:24 -0500

    feat: configuracion del server

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express'); 
+const morgan = require('morgan'); 
+const cors = require('cors');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+})); 
+
+app.use(express.json());
+
+module.exports = app;
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,11 @@
+const app = require("./app.js"); 
 const { conn } = require('./db.js');
-
 conn.sync({ force: true}).then(async() => {
     try {
         console.log("Database connected")
+        await app.listen(3000, () => {
+            console.log("server corriendo en el puerto 3000")
+        })
     } catch (error) {
         console.log("error", error)        
     }
